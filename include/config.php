a80093ea6eaa40a334f122cb56ad6d8dbc297798
alfianbhakti35 <alfianbhakti35@gmail.com>
2022-01-01 15:27:52 +0800

    first commit

---
+++ include/config.php
@@ -0,0 +1,5 @@
+<?php 
+if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mikhmon'] = array ('1'=>'mikhmon<|<mikhmon','mikhmon>|>aWNlbA==');
+
+$data['oweys'] = array ('1'=>'oweys!192.168.30.1','oweys@|@mikhmon','oweys#|#amFjamNiaWd4oZKgnJI=','oweys%Dafi Hotspot','oweys^dh.net','oweys&Rp','oweys*10','oweys(1','oweys)','oweys=10','oweys@!@enable');
---
+++ voucher/temp.php
@@ -0,0 +1,1 @@
+<?php $genu="rpRfbmhnZWJkZmNnZmNjZamqsKmqtK+Cal5kaGFitmaatmNiaGFTamFiaK9noK9itnagmZOenQ==";?>
